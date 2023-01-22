2f357a7515fa7f18d8042ea864a36671e02b23e3
Ishmam Ahmed <ishmam.ahmed24@gmail.com>
2023-01-22 20:49:33 +0000

    Formatted fonts of titles and captions

--- components/Banner.js
+++ components/Banner.js
@@ -10,13 +10,15 @@ const Banner = () => {
         <div className='max-w-[450px] text-white flex flex-col gap[40px]'>
 
           <div>
-            <h1 className="text-5xl">We are Metora Cliffs</h1>
-            <h4 className='text-2xl mt-3'>
+            <h2 className="text-4xl px-4">Welcome to</h2>
+            <h1 className="text-5xl px-4">Metora Cliffs</h1>
+            <h4 className='text-2xl mt-3 px-4'>
             <Typewriter
               options={{
                 strings: [
-                  "Leeds Based",
-                  "Prog Band"
+                  "Leeds-based",
+                  "Progressive Rock Band",
+                  "Video Game Metal"
                 ],
                 changeDelay: 3,
                 changeDeleteSpeed: 2,
@@ -27,8 +29,8 @@ const Banner = () => {
             </h4>
           </div>
           <p>&nbsp;</p>
-          <div className='bg-[#834333] h-[2px] w-[70px]'></div> <p>&nbsp;</p>
-          <div> <p>Cited as one of the bands of all time in Leeds</p>
+          <div className='bg-[#834333] h-[4px] w-[250px]'></div> <p>&nbsp;</p>
+          <div className='px-4'> <p>Cited as one of the bands of all time in Leeds.</p>
 
           </div>
 
