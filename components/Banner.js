dcea74c2efff2aeb17017a270e115ede67f9dc1d
Ishmam Ahmed <ishmam.ahmed24@gmail.com>
2023-01-25 16:44:47 +0000

    Added logo over bg

--- components/Banner.js
+++ components/Banner.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Image from 'next/image'
+import bannerpart from '../components/bannerpart.png'
 import Typewriter from "typewriter-effect"
 
 const Banner = () => {
   return (
     <div className='bg-[url("/metorabanner.jpg")] h-screen bg-cover bg-center bg-fixed flex items-center'>
-
+      
       <div className='container mx-auto px-4'>
 
         <div className='max-w-[450px] text-white flex flex-col gap[40px]'>
@@ -37,6 +39,7 @@ const Banner = () => {
 
         </div>
       </div>
+      <a className='px-10'> <Image src={bannerpart} width="300px" height="300px" alt="Metora Cliffs" /> </a>
     </div>
   )
 }
