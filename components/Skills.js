23ed5e92c0deb557bd8688f4740ad624545fe9a9
Ishmam Ahmed <ishmam.ahmed24@gmail.com>
2023-01-31 22:28:07 +0000

    Started Skill stats section

--- components/Skills.js
+++ components/Skills.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import Heading from './Heading'
 
 const Skills = () => {
   return (
-    <section className=''>
+    <section className='container mx-auto py-10 px-4' style={{ fontFamily: 'Adventure Subtitles', sansSerif: true }}>
+            <Heading title="Member's Skills" />
+            <p>&nbsp;</p>
+            <p className='text-gray-600 -mt-10 mb-10'>Each of our band members have unique skills and characteristics. Check them out!</p>
+
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
+
+                <div>
+                    <p>Chug</p>
+                    <div className='relative bg-gray-400 j-[4px] mt-2'>
+                        <div className='bg-[#ff4D41] absolute h-[4px] w-[60%]'>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
       
     </section>
   )
