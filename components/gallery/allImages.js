92e149270eb51f5bed572cf4b65bc68bafb9da81
Ishmam Ahmed <ishmam.ahmed24@gmail.com>
2023-04-22 08:13:44 +0100

    Cleaner image import implementation

---
+++ components/gallery/allImages.js
@@ -0,0 +1,77 @@
+import MCMC from '../MCMC.jpg';
+import ADWS from '../ADWS.png';
+import MOM from '../MOM.png';
+import a from './a.jpg';
+import b from './b.jpg';
+import c from './c.jpg';
+import d from './d.jpg';
+import e from './e.jpg';
+import f from './f.jpg';
+import g from './g.jpg';
+import h from './h.jpg';
+import i from './i.jpg';
+import j from './j.jpg';
+import k from './k.jpg';
+import l from './l.jpg';
+import m from './m.jpg';
+import n from './n.jpg';
+import o from './o.jpg';
+import p from './p.jpg';
+import q from './q.jpg';
+import r from './r.jpg';
+import s from './s.jpg';
+import t from './t.jpg';
+import u from './u.jpg';
+import v from './v.jpg';
+import w from './w.jpg';
+import x from './x.jpg';
+import y from './y.jpg';
+import z from './z.jpg';
+import aa from './aa.jpg';
+import bb from './bb.jpg';
+import cc from './cc.jpg';
+import dd from './dd.jpg';
+import ee from './ee.jpg';
+
+
+export const artworkImages = [
+  { src: MCMC, title: 'Project Title MCMC' },
+  { src: ADWS, title: 'Project Title ADWS' },
+  { src: MOM, title: 'Project Title MOM' },
+  { src: a, title: 'Project Title A' },
+  { src: b, title: 'Project Title B' },
+  { src: c, title: 'Project Title C' },
+  { src: d, title: 'Project Title D' },
+  { src: e, title: 'Project Title E' },
+];
+
+
+export const photographyImages = [
+  { src: f, title: 'Project Title F' },
+  { src: g, title: 'Project Title G' },
+  { src: h, title: 'Project Title H' },
+  { src: i, title: 'Project Title I' },
+  { src: j, title: 'Project Title J' },
+  { src: k, title: 'Project Title K' },
+  { src: l, title: 'Project Title L' },
+  { src: m, title: 'Project Title M' },
+  { src: n, title: 'Project Title N' },
+  { src: o, title: 'Project Title O' },
+  { src: p, title: 'Project Title P' },
+  { src: q, title: 'Project Title Q' },
+  { src: r, title: 'Project Title R' },
+  { src: s, title: 'Project Title S' },
+  { src: t, title: 'Project Title T' },
+  { src: u, title: 'Project Title U' },
+  { src: v, title: 'Project Title V' },
+  { src: w, title: 'Project Title W' },
+  { src: x, title: 'Project Title X' },
+  { src: y, title: 'Project Title Y' },
+  { src: z, title: 'Project Title Z' },
+  { src: aa, title: 'Project Title AA' },
+  { src: bb, title: 'Project Title BB' },
+  { src: cc, title: 'Project Title CC' },
+  { src: dd, title: 'Project Title DD' },
+  { src: ee, title: 'Project Title EE' },
+];
+
