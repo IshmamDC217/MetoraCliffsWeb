34bc9195a9effd7480bdd506ae3348f0eb08e763
Ishmam Ahmed <ishmam.ahmed24@gmail.com>
2023-04-25 14:53:13 +0100

    Border for Button

--- components/Button.js
+++ components/Button.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({ text, link }) => {
   return (
-    <a href={link} className='btn'>{text}</a>
+    <a href={link} className='btn border-white border-[1px]'>{text}</a>
   )
 }
 
