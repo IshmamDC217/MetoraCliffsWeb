30a7acc6463baca8e589a039fbf4fc2502aca89d
Ishmam Ahmed <ishmam.ahmed24@gmail.com>
2023-04-25 16:25:22 +0100

    netlify test 1

--- components/Navbar.js
+++ components/Navbar.js
@@ -18,7 +18,7 @@ const Navbar = () => {
     };
 
     return (
-        <div className='bg-white fixed top-0 w-[100%] z-10 px-10'>
+        <div className='bg-white fixed top-0 w-[100%] z-10 p-10'>
             <div className='container mx-auto flex justify-between items-center py-4 px-4 ' style={{ fontFamily: 'Adventure Subtitles', sansSerif: true }}>
                 <div className='max-w-xs'> <Image src={corner} width="300px" height="300px" alt="Metora Cliffs" /> </div>
                 <div className='hidden md:flex gap-6'>
