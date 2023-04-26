305bad2cb70e5b4b26ab09197e7811006e3981db
Ishmam Ahmed <ishmam.ahmed24@gmail.com>
2023-04-26 10:03:28 +0100

    Heading(s) fixed

--- components/Heading.js
+++ components/Heading.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Heading = ({ title }) => {
   return (
-    <div className='text-[#ffffff] text-[32px] font-medium flex items-center gap-4 
+    <div className='text-white text-2xl font-medium flex items-center gap-4 
     py-5'>
       {title}
       <div className='bg-[#7aace8] h-[2px] w-[40px] translate-y-1'></div>
