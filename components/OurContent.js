7930af459f99d0afd5fc1e4aad142bc437f6622f
Ishmam Ahmed <ishmam.ahmed24@gmail.com>
2023-04-26 12:06:00 +0100

    Colors for content fixed: web ready

--- components/OurContent.js
+++ components/OurContent.js
@@ -16,11 +16,11 @@ const OurContent = () => {
             <Heading title="Our Content" />
             <div ref={ref} className={`${inView ? 'show' : null} intobs grid grid-cols-1 md:grid-cols-3 gap-6`}>
                 <a class="scale-90 hover:scale-100 ease-in duration-200" href="https://open.spotify.com/artist/7L8GCBu7rpT4nWglH7d0X6">
-                    <div className='max-w-[500px] shadow-lg flex flex-col gap-4 px-8 py-10 bg-[#e95550] text-white'>
+                    <div className='max-w-[500px] shadow-lg flex flex-col gap-4 px-8 py-10 text-white' style={{ backgroundColor: '#e95550'}}>
 
-                        <SiYoutubemusic className='text-[28px]' />
+                        <SiYoutubemusic style={{ fontSize: '28px'}}/>
 
-                        <h2 className='font-medium text-[28px]'>Music</h2>
+                        <h2 className='font-medium text-2xl'>Music</h2>
                         <Image class="image" src={tm} alt="" />
 
                         <p>Our Originals</p>
@@ -28,11 +28,11 @@ const OurContent = () => {
                 </a>
 
                 <a class="scale-90 hover:scale-100 ease-in duration-200" href="https://www.youtube.com/@metoracliffs5994">
-                    <div className='max-w-[500px] shadow-lg flex flex-col gap-4 px-8 py-10 bg-[#feb833] text-white'>
+                    <div className='max-w-[500px] shadow-lg flex flex-col gap-4 px-8 py-10 text-white' style={{ backgroundColor: '#feb833'}}>
 
-                        <BsFillCameraVideoFill className='text-[28px]' />
+                        <BsFillCameraVideoFill style={{ fontSize: '28px'}}/>
 
-                        <h2 className='font-medium text-[28px]'>Videos</h2>
+                        <h2 className='font-medium text-2xl'>Videos</h2>
                         <Image class="image" src={tv} alt="" />
                         <p>Covers and Music Videos</p>
 
@@ -40,11 +40,11 @@ const OurContent = () => {
                 </a>
 
                 <a class="scale-90 hover:scale-100 ease-in duration-200" href="">
-                    <div className='max-w-[500px] shadow-lg flex flex-col gap-4 px-8 py-10 bg-[#48929c] text-white'>
+                    <div className='max-w-[500px] shadow-lg flex flex-col gap-4 px-8 py-10 text-white' style={{ backgroundColor: '#48929c'}}>
 
-                        <BsMusicNoteList className='text-[28px]' />
+                        <BsMusicNoteList style={{ fontSize: '28px'}}/>
 
-                        <h2 className='font-medium text-[28px]'>Tabs</h2>
+                        <h2 className='font-medium text-2xl'>Tabs</h2>
                         <Image class="image" src={tt} alt="" />
                         <p>Tabs for our songs</p>
 
