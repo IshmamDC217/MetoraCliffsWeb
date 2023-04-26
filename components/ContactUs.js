4a468953f11e147596884be4b69c5ee07a24d9ba
Ishmam Ahmed <ishmam.ahmed24@gmail.com>
2023-04-26 15:22:36 +0100

    CSS Fix

--- components/ContactUs.js
+++ components/ContactUs.js
@@ -78,7 +78,7 @@ const ContactUs = () => {
         <p>&copy; 2023 Dylan Tann, Metora Cliffs. All rights reserved</p>
       </div>
       <div className="dyllicense contact-footer mt-10 text-white">
-        <p>&nbsp; &copy; 2023 Dylan Tann, Metora Cliffs. &nbsp; &nbsp; &nbsp; All rights reserved</p>
+        <p>&nbsp; &copy; 2023 Dylan Tann, Metora Cliffs. All rights reserved</p>
         {/* <p>Designed and Developed by Ishmam Ahmed.</p> */}
       </div>
     </section>
