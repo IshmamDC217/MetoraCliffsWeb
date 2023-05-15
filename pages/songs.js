8fbf7b1090b02828caf80a58b3a741fbd5acada4
Ishmam Ahmed <ishmam.ahmed24@gmail.com>
2023-05-15 11:54:24 +0100

    layout fix and sidebar imp

--- pages/songs.js
+++ pages/songs.js
@@ -3,8 +3,16 @@ import Navbar from "../components/Navbar"
 import OurContent from '../components/OurContent'
 import ParticlesBG from '../components/ParticlesBG'
 import Footer from '../components/Footer'
+import Sidemenu from '../components/Sidemenu'
+import { useState } from 'react'
 
 export default function GalleryPage() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div>
       <Head>
@@ -14,14 +22,36 @@ export default function GalleryPage() {
       </Head>
 
       <header>
-        <Navbar />
+        <Navbar toggleSidebar={toggleSidebar} />
       </header>
 
       <main>
-        <OurContent/>
+        <button onClick={toggleSidebar}>Toggle Sidebar</button>
+        {isOpen && (
+          <div
+            style={{
+              position: 'fixed',
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              backgroundColor: 'rgba(0, 0, 0, 0.5)',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              zIndex: 9999,
+            }}
+          >
+            <Sidemenu isOpen={isOpen} toggleSidebar={toggleSidebar} />
+          </div>
+        )}
+        <OurContent />
         <ParticlesBG />
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
         <Footer />
       </main>
     </div>
   )
-  }
+}
