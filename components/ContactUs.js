ace28a29806b18663c8dc4aa099ccd78fb40ad7c
Ishmam Ahmed <ishmam.ahmed24@gmail.com>
2023-06-04 22:29:18 +0100

    Form fix (final)

--- components/ContactUs.js
+++ components/ContactUs.js
@@ -50,13 +50,13 @@ const ContactUs = () => {
         <div className="contact-form-and-image flex flex-wrap justify-center mt-10 gap-10 mr-1.5 ml-1">
           <div className="contactus bg-[#58629c] p-14 py-16 rounded-md border w-[75%] mb-10" whileHover={{ y: -5 }}>
             <h2 className="text-2xl text-white font-bold mb-6">Get in touch with us!</h2>
-            <form name="contact" method="POST" data-netlify="true" onSubmit="submit">
+            <form name="contact" method="POST" data-netlify="true">
               <input type="hidden" name="form-name" value="contact" />
               <div whileHover={{ scale: 1.02 }} className="flex flex-col mb-3">
                 <label htmlFor="name" className="text-white mb-1">
                   Name
                 </label>
-                <input type="text" id="name" className="border border-gray-300 p-2 rounded" />
+                <input type="text" id="name" name="name" className="border border-gray-300 p-2 rounded" />
               </div>
               <div whileHover={{ scale: 1.02 }} className="flex flex-col mb-3">
                 <label htmlFor="email" className="text-white mb-1">
@@ -65,6 +65,7 @@ const ContactUs = () => {
                 <input
                   type="email"
                   id="email"
+                  name="email"
                   whileHover={{ scale: 1.02 }}
                   className="border border-gray-300 p-2 rounded"
                 />
@@ -73,15 +74,13 @@ const ContactUs = () => {
                 <label htmlFor="message" className="text-white mb-1">
                   Message
                 </label>
-                <textarea id="message" rows="4" className="border border-gray-300 p-2 rounded"></textarea>
+                <textarea id="message" name="message" rows="4" className="border border-gray-300 p-2 rounded"></textarea>
               </div>
-              <button
-                type="submit"
-                className="bg-gray-800 text-white py-2 px-4 rounded mt-6"
-              >
+              <button type="submit" className="bg-gray-800 text-white py-2 px-4 rounded mt-6">
                 Send Message
               </button>
             </form>
+
             <div className="contact-socials text-white flex justify-center items-center gap-5 mt-10">
               <p>Alternatively, you can contact us on:</p>
               <a href="https://facebook.com/metoracliffs" rel="noopener noreferrer">
