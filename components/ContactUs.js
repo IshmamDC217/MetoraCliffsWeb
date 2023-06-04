77ce31ca1fae65e36c6f6a41bdc73ba5d8bef8d4
Ishmam Ahmed <ishmam.ahmed24@gmail.com>
2023-06-04 17:17:35 +0100

    Netlify forms imp

--- components/ContactUs.js
+++ components/ContactUs.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import CustomNotification from './CustomNotification';
-import { motion } from "framer-motion";
-import Image from "next/image"
-import MCbandpic from "../components/MCbandpic.jpg"
-import Heading from "./Heading"
-import { FaFacebook, FaInstagram, FaTiktok } from "react-icons/fa";
-import nbandpic from '../components/gallery/nbandpic.jpg'
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+import MCbandpic from '../components/MCbandpic.jpg';
+import Heading from './Heading';
+import { FaFacebook, FaInstagram, FaTiktok } from 'react-icons/fa';
+import nbandpic from '../components/gallery/nbandpic.jpg';
 
 const ContactUs = () => {
   const [showNotification, setShowNotification] = useState(false);
@@ -38,29 +38,27 @@ const ContactUs = () => {
       {/* <div className="notif">
         {showNotification && (
           <CustomNotification
-            message="🖼️ Don&apos;t miss out on our Gallery!"
+            message="🖼️ Don't miss out on our Gallery!"
             onClose={() => setShowNotification(false)}
           />
         )}
       </div> */}
       <section className="contactcontainer mx-auto py-16 mt-20">
-
-        <h1 className='text-3xl' style={{ fontFamily: 'Adventure Subtitles', sansSerif: true }}>- Contact Us -</h1>
+        <h1 className="text-3xl" style={{ fontFamily: 'Adventure Subtitles', sansSerif: true }}>
+          - Contact Us -
+        </h1>
         <div className="contact-form-and-image flex flex-wrap justify-center mt-10 gap-10 mr-1.5 ml-1">
           <motion.div className="contactus bg-[#58629c] p-14 py-16 rounded-md border w-[75%] mb-10" whileHover={{ y: -5 }}>
-            <h2 className="text-2xl text-white font-bold mb-6">Don&apos;t worry, we don&apos;t bite!</h2>
-            <form className="space-y-4">
-              <motion.div whileHover={{ scale: 1.02 }} className="flex flex-col">
+            <h2 className="text-2xl text-white font-bold mb-6">Don't worry, we don't bite!</h2>
+            <form name="contact" method="POST" data-netlify="true">
+              <input type="hidden" name="form-name" value="contact" />
+              <motion.div whileHover={{ scale: 1.02 }} className="flex flex-col mb-3">
                 <label htmlFor="name" className="text-white mb-1">
                   Name
                 </label>
-                <input
-                  type="text"
-                  id="name"
-                  className="border border-gray-300 p-2 rounded"
-                />
+                <input type="text" id="name" className="border border-gray-300 p-2 rounded" />
               </motion.div>
-              <motion.div whileHover={{ scale: 1.02 }} className="flex flex-col">
+              <motion.div whileHover={{ scale: 1.02 }} className="flex flex-col mb-3">
                 <label htmlFor="email" className="text-white mb-1">
                   Email
                 </label>
@@ -75,11 +73,7 @@ const ContactUs = () => {
                 <label htmlFor="message" className="text-white mb-1">
                   Message
                 </label>
-                <textarea
-                  id="message"
-                  rows="4"
-                  className="border border-gray-300 p-2 rounded"
-                ></textarea>
+                <textarea id="message" rows="4" className="border border-gray-300 p-2 rounded"></textarea>
               </motion.div>
               <motion.button
                 variants={buttonVariants}
@@ -91,17 +85,23 @@ const ContactUs = () => {
               </motion.button>
               <div className="contact-socials text-white flex justify-center items-center gap-5 mt-10">
                 <p>Alternatively, you can contact us on:</p>
-                <a href="https://facebook.com/metoracliffs" rel="noopener noreferrer"><FaFacebook /></a>
-                <a href="https://instagram.com/metoracliffsmusic" rel="noopener noreferrer"><FaInstagram /></a>
-                <a href="https://tiktok.com/@metoracliffs" rel="noopener noreferrer"><FaTiktok /></a>
+                <a href="https://facebook.com/metoracliffs" rel="noopener noreferrer">
+                  <FaFacebook />
+                </a>
+                <a href="https://instagram.com/metoracliffsmusic" rel="noopener noreferrer">
+                  <FaInstagram />
+                </a>
+                <a href="https://tiktok.com/@metoracliffs" rel="noopener noreferrer">
+                  <FaTiktok />
+                </a>
               </div>
             </form>
           </motion.div>
-          <div style={{ display: 'flex', flexDirection: "column" }}>
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
             <div className="license contactus-image max-w-sm w-half">
               <Image src={MCbandpic} alt="" className="rounded-lg mb-10" />
             </div>
-            
+
             <div className="license contactus-image max-w-sm w-half">
               <Image src={nbandpic} alt="" className="rounded-lg" />
             </div>
@@ -118,4 +118,5 @@ const ContactUs = () => {
     </>
   );
 };
+
 export default ContactUs;
